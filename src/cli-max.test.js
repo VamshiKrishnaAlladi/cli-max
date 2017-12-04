5a5e62e3a0a8e0fdb2414a45385282e9e80c5fae
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2017-12-04 12:34:50 +0530

    Moved index.js and index.test.js to "src" as cli-max.js and cli-max.test.js

--- src/cli-max.test.js
+++ src/cli-max.test.js
@@ -2,7 +2,7 @@
 
 describe('Module cli-max:', () => {
     test('should export an object with a "configure" method', () => {
-        const cli = require('./index');
+        const cli = require('./cli-max');
 
         expect(cli).toHaveProperty('configure');
         expect(cli.configure).toBeInstanceOf(Function);
