60efaf7dc0a782ff7a48a030281828d33bede615
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2018-01-15 11:06:26 +0530

    Updated the implementation of CLIMax to throw ForbiddenActionErrror when instantiated

--- cli-max/cli-max.js
+++ cli-max/cli-max.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var forbidden_action_error_1 = require("@vka/ts-utils/errors/forbidden-action-error");
 var CLIMax = /** @class */ (function () {
     function CLIMax() {
-        throw new Error('CLIMax is intended to be used as a Static class. Do not instantiate it.');
+        throw new forbidden_action_error_1.ForbiddenActionError('CLIMax is intended to be used as a Static class. Do not instantiate it.');
     }
     return CLIMax;
 }());
--- cli-max/cli-max.ts
+++ cli-max/cli-max.ts
@@ -1,5 +1,7 @@
+import { ForbiddenActionError } from '@vka/ts-utils/errors/forbidden-action-error';
+
 export class CLIMax {
     constructor() {
-        throw new Error('CLIMax is intended to be used as a Static class. Do not instantiate it.');
+        throw new ForbiddenActionError('CLIMax is intended to be used as a Static class. Do not instantiate it.');
     }
 }
