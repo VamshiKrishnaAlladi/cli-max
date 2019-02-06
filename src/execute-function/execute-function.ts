393fa8ac041b482806199b0bbb52904dcc7e5e65
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2019-02-07 00:15:07 +0530

    feat(command): implement command aliases
    fixes #82

--- src/execute-function/execute-function.ts
+++ src/execute-function/execute-function.ts
@@ -4,27 +4,20 @@ import * as minimist from 'minimist';
 import { Command } from '../command';
 import { processArgs } from '../command-options';
 
-interface CommandMap {
-    [commandName: string]: Command;
-}
-
 export type ExecuteFn = (args?: string[]) => any;
 
 export function createExecuteFn(commands: Command[] = mandate('commands')): ExecuteFn {
-    const [defaultCommand] = commands.filter(command => command.isDefault);
-
-    const commandMap: CommandMap = commands.reduce((commandMap, command) => {
-        commandMap[command.name] = command;
-        return commandMap;
-    }, {});
-
     return (args: string[] = mandate('args')) => {
+        const [defaultCommand] = commands.filter(command => command.isDefault);
+
         const {
             _: [commandName = defaultCommand.name, ...subCommands],
             ...remainingArgs
         } = minimist(args);
 
-        const commandToExecute = commandMap[commandName] || defaultCommand;
+        const [commandToExecute = defaultCommand] = commands.filter(({ name, aliases = [] }) => {
+            return name === commandName || aliases.includes(commandName);
+        });
 
         const processedArgs = processArgs(remainingArgs, commandToExecute.options);
 
